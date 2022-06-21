9f9fe5eb146e7a7d807d26f3d75bb36e4d25dc2c
snnbotchway <solomonbotchway7@gmail.com>
2022-06-21 17:02:47 +0000

    Fixed the homepage to display images

--- feed/views.py
+++ feed/views.py
@@ -5,6 +5,11 @@ from .models import Post
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['img_obj'] = Post.objects.all()
+        return context
+    
 
 class NewImageView(FormView):
     template_name= 'new-image.html'
