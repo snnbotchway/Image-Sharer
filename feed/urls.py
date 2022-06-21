598e41ace4e39d3cf4a357093d3c8f25a66a3ac4
snnbotchway <solomonbotchway7@gmail.com>
2022-06-21 23:41:16 +0000

    Added a detail view

--- feed/urls.py
+++ feed/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import HomePageView,NewImageView
+from .views import HomePageView,NewImageView, ImageDetail
 
 app_name = 'feed'
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('home/', HomePageView.as_view(), name='home'),
     path('index/', HomePageView.as_view(), name='home'),
     path('upload/', NewImageView.as_view(), name='upload'),
+    path('detail/<int:pk>', ImageDetail.as_view(), name='detail'),
 ]
