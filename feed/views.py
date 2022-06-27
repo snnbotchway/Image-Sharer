fe3cfc47e7690aa9be135901ca88506fa5dc6ed8
snnbotchway <solomonbotchway7@gmail.com>
2022-06-27 19:45:39 +0000

    Added messages after uploads

--- feed/views.py
+++ feed/views.py
@@ -1,13 +1,14 @@
 from django.views.generic import TemplateView, FormView, DetailView
 from .forms import AddNewImage
 from .models import Post
+from django.contrib import messages
 
 # Create your views here.
 class HomePageView(TemplateView):
     template_name = 'home.html'
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['img_obj'] = Post.objects.all()
+        context['img_obj'] = Post.objects.all().order_by('-id')
         return context
 
 class NewImageView(FormView):
@@ -15,12 +16,18 @@ class NewImageView(FormView):
     form_class= AddNewImage
     success_url= '/'
 
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
         Post.objects.create(
             description = form.cleaned_data['description'],
             image = form.cleaned_data['image']
         )
+        messages.add_message(self.request, messages.SUCCESS, 'Image uploaded successfully!')
         return super().form_valid(form)
+    
 
 
 class ImageDetail(DetailView):
